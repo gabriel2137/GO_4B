151d0e00fdffff3f432c46152d6ed26072b9c98a
Jan Kowalski <mail@gmail.com>
2023-10-03 10:57:01 +0200

    Initial commit

---
+++ src/Exampel.java
@@ -0,0 +1,14 @@
+public class Exampel {
+    public static void main(String[] args) {
+        System.out.println("czes");
+
+        int liczba = 9;
+        double liczba2 = 12.7;
+        System.out.println("moja liczba " + liczba2);
+        boolean = true;
+        char x = 'f';
+        String napis = "uj" +
+                "";
+        int dlugosc = napis.length;
+    }
+}
